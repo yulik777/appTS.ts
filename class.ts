6a96b6028bee9ea723c89edefd8a9ac936b8638a
Yuliia Skabitskaya <yulik777@i.ua>
2022-10-23 19:06:58 +0300

    classes

---
+++ class.ts
@@ -0,0 +1,42 @@
+class UserBB {
+    name: string;
+
+    constructor(name: string){
+    this.name = name;
+    }
+}
+
+const uuser = new UserBB('Yulik');
+console.log(uuser);
+uuser.name = 'yulia'; // присвоили новое имя
+console.log(uuser);
+
+class Adminn {
+role!: number; //!: - так надо 
+}
+
+const adminn = new Adminn();
+adminn.role = 1;
+
+//Конструктор
+
+class User6 {
+    name!: string;
+    age!: number;
+
+constructor();
+constructor(name: string);
+constructor(age: number);
+
+constructor(Ageorname?: string | number){
+    if(typeof Ageorname === 'string'){
+    this.name = Ageorname;
+    } else if
+    (typeof Ageorname === 'number')
+    this.age = Ageorname;
+}
+}
+
+const user6 = new User6('Yulik');
+const useeer = new User6();
+const userrr = new User6(33);

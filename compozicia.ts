4894d46591ac4a7d1f865c99128fec1acd425479
Yuliia Skabitskaya <yulik777@i.ua>
2022-10-25 21:10:37 +0300

    composition

---
+++ compozicia.ts
@@ -0,0 +1,35 @@
+class Usxer {
+    name: string;
+
+    constructor( name: string){
+        this.name = name;
+    }
+}
+
+class Usxers extends Array<Usxer>{// наследуют массив пользователей
+searchByName(name: string) {
+    return this.filter( u => u.name === name);}
+
+override toString(): string {
+    return this.map(u => u.name).join(', ')
+   }
+}
+
+
+const usxers = new Usxers();
+usxers.push(new Usxer('Vas'));
+usxers.push(new Usxer('Max'));
+console.log(usxers.toString()); //Vas, Max
+
+//правильная связка юзера и пеймента 
+//такая композиция упростит код и уменьшит связанность user payment
+
+class UserwithPayment {
+    user: User;
+    payment: Payment;
+
+    constructor(user: User, payment: Payment){
+        this.payment = payment;
+        this.user = user;
+    }
+}

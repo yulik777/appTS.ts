6509be0b991c70ba10a4de6f5cfbfeb33a966cbd
Yuliia Skabitskaya <yulik777@i.ua>
2022-11-03 19:45:59 +0200

    gnrc

--- gnrc_function.ts
+++ gnrc_function.ts
@@ -10,3 +10,29 @@ function getSplitedHalf<T>(data: Array<T>): Array<T> {
     return data.splice(0, 1); // делим пополам
 }
 getSplitedHalf<number>([1, 3, 4]);
+
+// Function которая принимает тип и возвращает сроку
+
+function toString<T>(data: T): string | undefined{
+if (Array.isArray(data)) {
+    return data.toString();
+}
+switch (typeof data){
+    case 'string':
+        return data;
+        case 'number':
+        case'symbol':
+        case'bigint':
+        case 'boolean':
+        case 'function':
+            return data.toString();
+    case 'object':
+        return JSON.stringify(data);
+    default:
+        return undefined;       
+}
+}
+console.log(toString(3));
+console.log(toString(true));
+console.log(toString(['a', 'b']));
+console.log(toString({a: 1}));

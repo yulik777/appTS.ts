d0b36e1934f45a7b84674f1fab3074d0d7e3e5ad
Yuliia Skabitskaya <yulik777@i.ua>
2022-12-15 21:52:32 +0200

    fix

--- never.ts
+++ never.ts
@@ -8,3 +8,22 @@ function dumpError(): never{
 
 //const a: never = 1; ne mozem prisvoit
 
+type paymentAction = 'refund' | 'checkout' | 'reject';
+
+function processAction(action: paymentAction){
+    switch (action) {
+        case 'refund':
+            //....
+            break;
+            case 'checkout':
+                //....
+                break;
+                case 'reject':
+                    //....
+                    break; 
+                default:
+                    const _: never = action; // proverka
+                    throw new Error('No such action');
+    }
+}
+

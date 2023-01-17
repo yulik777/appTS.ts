37305392ebc55c2506c91b33258fa41f932bda64
Yuliia Skabitskaya <yulik777@i.ua>
2023-01-17 14:29:11 +0200

    compozition

--- compozicia.ts
+++ compozicia.ts
@@ -24,13 +24,4 @@ console.log(usxers.toString()); //Vas, Max
 //правильная связка юзера и пеймента 
 //такая композиция упростит код и уменьшит связанность user payment
 
-class UserwithPayment {
-    user: User;
-    payment: Payment;
-
-    constructor(user: User, payment: Payment){
-        this.payment = payment;
-        this.user = user;
-    }
-}
 
